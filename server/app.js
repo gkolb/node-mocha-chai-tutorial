b974dde0e2effabd2484961f17e65a410b1d543d
gkolb <gkolb100@gmail.com>
2018-05-02 12:29:40 -0600

    Add testing database and hooks to test-server.js

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
 var mongoose = require('mongoose');
+var config = require('./_config');
 
 // *** routes *** //
 var routes = require('./routes/index.js');
@@ -14,11 +15,11 @@ var routes = require('./routes/index.js');
 var app = express();
 
 // *** mongoose *** ///
-mongoose.connect('mongodb://localhost/node-testing', function(err, res) {
+mongoose.connect(config.mongoURI[app.settings.env], function(err, res) {
   if(err) {
     console.log('Error connecting to the database. ' + err);
   } else {
-    console.log('Connected to Database!');
+    console.log('Connected to Database: ' + config.mongoURI[app.settings.env]);
   }
 });
 
--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,34 @@
+process.env.NODE_ENV = 'test';
+
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server/app');
-var should = chai.should();
+var mongoose = require("mongoose");
 
+var server = require('../server/app');
+var Blob = require("../server/models/blob");
+
+var should = chai.should();
 chai.use(chaiHttp);
 
+
 describe('Blobs', function() {
+
+  Blob.collection.drop();
+
+  beforeEach(function(done){
+    var newBlob = new Blob({
+      name: 'Bat',
+      lastName: 'man'
+    });
+    newBlob.save(function(err) {
+      done();
+    });
+  });
+  afterEach(function(done){
+    Blob.collection.drop();
+    done();
+  });
+
   it('should list ALL blobs on /blobs GET', function(done) {
     chai.request(server)
       .get('/blobs')
@@ -13,8 +36,35 @@ describe('Blobs', function() {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
+        res.body[0].should.have.property('_id');
+        res.body[0].should.have.property('name');
+        res.body[0].should.have.property('lastName');
+        res.body[0].name.should.equal('Bat');
+        res.body[0].lastName.should.equal('man');
         done();
       });
+  });
+  it('should list a SINGLE blob on /blob/<id> GET', function(done) {
+    var newBlob = new Blob({
+      name: 'Super',
+      lastName: 'man'
+    });
+    newBlob.save(function(err, data) {
+      chai.request(server)
+        .get('/blob/'+data.id)
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('_id');
+          res.body.should.have.property('name');
+          res.body.should.have.property('lastName');
+          res.body.name.should.equal('Super');
+          res.body.lastName.should.equal('man');
+          res.body._id.should.equal(data.id);
+          done();
+        });
+    });
   });
   it('should add a SINGLE blob on /blobs POST', function(done) {
     chai.request(server)
@@ -34,7 +84,6 @@ describe('Blobs', function() {
         done();
       });
   });
-  it('should add a SINGLE blob on /blobs POST');
   it('should update a SINGLE blob on /blob/<id> PUT');
   it('should delete a SINGLE blob on /blob/<id> DELETE');
 });
